a978fc99bd9e0e71d2a9c359d46dcc3ff9b0eecd
Valerio Pipolo <valep27@gmail.com>
2018-05-13 19:52:27 +0200

    reorder root middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express'
 import bodyParser from 'body-parser'
 
-import initRoutes from './routes/index'
+import initRoutes from './routes'
 
 // Create Express server
 const app = express()
@@ -13,16 +13,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 initRoutes(app)
 
+// 404 handler
+app.use((_req: Request, res: Response, _next: NextFunction) => {
+  res.status(404).send()
+})
+
+// error handler
 app.use((error: any, _req: Request, res: Response, next: NextFunction) => {
   if (!error) {
     return next()
   }
 
-  res.status(500).send()
-})
-
-app.use((_req: Request, res: Response, _next: NextFunction) => {
-  res.status(404).send()
+  res.status(500).send(error)
 })
 
 export default app
